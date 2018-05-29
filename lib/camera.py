b2584594f67a8fe8ee1de84bd3967f2e6277a91b
GAS Team <getawayspecial@gmail.com>
2018-05-29 18:52:19 +0000

    cleaned up directory creation

--- lib/camera.py
+++ lib/camera.py
@@ -1,14 +1,18 @@
 # simple class to house code for the picamera
 
+import os
 import picamera
 from time import time
 
 class camera:
 
-    def __init__(self, directory = "./TSat_Pics/"):
+    def __init__(self, directory = "./data/TSat_Pics/"):
         self.camera = picamera.PiCamera()
         self.dir = directory
         self.count = 0
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
         
     def takePicture(self):
         self.camera.capture(self.dir + "image_" + str(self.count) + ".jpg")
--- tiny_sat.py
+++ tiny_sat.py
@@ -11,8 +11,8 @@ from time import time
 
 # constants
 RESERVED = 7  # number of values for median filter
-LO_PRES = 15  # for pressure range, low value
-HI_PRES = 35  # for pressure range, high value
+LO_PRES = 830  # for pressure range, low value
+HI_PRES = 860  # for pressure range, high value
 TIME = 60     # in seconds, for picture taking delay
 
 # initialization of components
@@ -22,7 +22,7 @@ baro_sensor = barometer()
 elapsed_clk = clock()
 
 # opening file
-data = file("data/data.txt", "w")
+data = file("data/data.csv", "w")
 
 # some helper variables for image taking
 take_picture  = False
@@ -31,11 +31,11 @@ previous_time = 0
 
 # initial output
 output = ""
-output += format("Pressure", "<10s")
-output += format("TempC", "<10s")
-output += format("TempF", "<10s")
-output += format("Altitude", "<10s")
-output += format("Time", "<10s")
+output += format("Pressure" + "\t")
+output += format("   TempC" + "\t")
+output += format("   TempF" + "\t")
+output += format("Altitude" + "\t")
+output += format("    Time" + "\t")
 
 # TODO: currently printing to console, but need to print to a tabbed file
 print(output)
@@ -48,6 +48,7 @@ data.write("\n")
 # 3. if pressure is in correct range, boom is deployed
 # 4. if boom was deployed, picture is taken every TIME miliseconds
 while True:
+    # reading from components
     baro_sensor.readBaro()
     med_filter.add(baro_sensor.getPressure())
     elapsed_clk.readTime()
@@ -55,15 +56,15 @@ while True:
     # get median
     median = med_filter.median()
 
+    # determine output values
     output = ""
-    output += format("%.2f " % baro_sensor.getPressure(), "<10s")
-    output += format("%.2f " % baro_sensor.getTemperatureC(), "<10s")
-    output += format("%.2f " % baro_sensor.getTemperatureF(), "<10s")
-    output += format("%.2f " % baro_sensor.getAltitude(median), "<10s")
+    output += format("%8.2f " % baro_sensor.getPressure() + "\t")
+    output += format("%8.2f " % baro_sensor.getTemperatureC() + "\t")
+    output += format("%8.2f " % baro_sensor.getTemperatureF() + "\t")
+    output += format("%8.2f " % baro_sensor.getAltitude(median) + "\t")
     output += format("%02d:%02d:%02d" % (elapsed_clk.getHours(),
             elapsed_clk.getMinutes(),
-            elapsed_clk.getSeconds()),
-            "<10s")
+            elapsed_clk.getSeconds()) + "\t")
 
     # TODO: currently printing to console, but need to print to a tabbed file
     print(output)
@@ -86,4 +87,4 @@ while True:
             elapsed_time += current_time - previous_time
             previous_time = current_time
 
-    sleep(2)  # sleep for 1 second
+    sleep(2)  # sleep for 2 seconds
