06fb4ec997050749195656f117497bd21423a5c3
Hailee Maxwell <hailee.maxwell@gmail.com>
2018-05-30 14:48:59 -0600

    some camera error handling

--- lib/camera.py
+++ lib/camera.py
@@ -2,17 +2,32 @@
 
 import os
 import picamera
+import subprocess
 import time
 
 class camera:
 
     def __init__(self, directory = "./data/TSat_Pics/"):
-        self.camera = picamera.PiCamera()
-        self.dir = directory
+        # run subprocess to see if a camera is connected
+        process = subprocess.Popen(["vcgencmd", "get_camera"],
+                stdout=subprocess.PIPE)
+        out, err = process.communicate()
+        
+        self.camera_connected = false
+        if "detected=1" in out:
+            self.camera_connected = true
 
+        # if camera is connected, initialize camera
+        if self.camera_connected:
+            self.camera = picamera.PiCamera()
+            self.dir = directory
+
+        # if directory does not already exist, create it
         if not os.path.exists(directory):
             os.makedirs(directory)
         
     def takePicture(self):
-        # done in case of restart
-        self.camera.capture(self.dir + "image_" + str(int(time.time())) + ".jpg")
+        # take a picture if camera is connected
+        if self.camera_connected:
+            # done in case of restart
+            self.camera.capture(self.dir + "image_" + str(int(time.time())) + ".jpg")
