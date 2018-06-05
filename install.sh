ef0ebb7c58e6d6569d491f0ac235b879d8f1c1e1
Carson Fox <carson.fox@aggiemail.usu.edu>
2018-06-04 22:04:06 -0600

    Install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Installing python script to run on boot"
+
+mkdir -p /etc/service/tsat
+
+echo '#!/bin/bash' > /etc/service/tsat/run
+echo 'exec /usr/bin/python /home/pi/TSat/tiny_sat.py' >> /etc/service/tsat/run
+
+chmod u+x /etc/service/tsat/run
+
